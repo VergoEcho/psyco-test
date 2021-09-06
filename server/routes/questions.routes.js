0f814a3efcf4549e63a79a0297383fabaf1270da
VergoEcho <52073488+VergoEcho@users.noreply.github.com>
2021-09-06 13:21:02 +0300

    LOG, not print

--- server/routes/questions.routes.js
+++ server/routes/questions.routes.js
@@ -29,7 +29,7 @@ router.post("/saveTestResults", async (req, res) => {
     console.log('after save');
     res.sendStatus(201);
   } catch (error) {
-    print('error: ',error)
+    console.log('error: ',error)
     res.status(500).json(error);
   }
 });
