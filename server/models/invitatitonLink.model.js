f8578a4c52fbb870e73f4b9a82c70c69674d537f
Vergo <vergo.echo@gmail.com>
2021-09-25 08:33:47 +0300

    Added date field to invitationLinkModel

--- server/models/invitatitonLink.model.js
+++ server/models/invitatitonLink.model.js
@@ -1,10 +1,20 @@
 const { Schema, model } = require("mongoose");
 
 const invitationLinkSchema = new Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  // createdAt: { type: Date, expires: "2m", default: Date.now },
   link: {
     type: String,
     required: true,
   },
 });
+
+// invitationLinkSchema.createIndex(
+//   { createdAt: 1 },
+//   { expireAfterSeconds: 3600 }
+// );
 
 module.exports = model("invitationLinks", invitationLinkSchema);
